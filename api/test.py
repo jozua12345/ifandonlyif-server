38786b3acfe307f742ee2f39d920cded36c44ffc
jozua12345 <jozua12345@outlook.com>
2020-03-09 21:01:04 +0800

    Reset User

--- api/test.py
+++ api/test.py
@@ -4,9 +4,11 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 django.setup()
 import json
-from deals.models import Requests
+from deals.models import Requests, ClientUsers
 from datetime import date
 
 
 queryset = Requests.objects.all()
 queryset.delete()
+queryset2 = ClientUsers.objects.all()
+queryset2.delete()
