09c4cd7b9bfb6c35e7e6a54d3cd2fe5094f476c1
jozua12345 <jozua12345@outlook.com>
2020-03-17 22:16:02 +0800

    fix

--- api/onepair_crawler/onepair_crawler/settings.py
+++ api/onepair_crawler/onepair_crawler/settings.py
@@ -21,9 +21,7 @@ NEWSPIDER_MODULE = 'onepair_crawler.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) 
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 
-Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
