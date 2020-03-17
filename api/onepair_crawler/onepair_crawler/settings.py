e51c7eed22188c77c89f7cf176a62a5f50e61f71
jozua12345 <jozua12345@outlook.com>
2020-03-17 21:42:13 +0800

    IGNORE ROBOT TXT :)

--- api/onepair_crawler/onepair_crawler/settings.py
+++ api/onepair_crawler/onepair_crawler/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'onepair_crawler.spiders'
 #USER_AGENT = 'onepair_crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
